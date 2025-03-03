205145bacfaca141c86147ee2e37910c84c7949e
savasdn <githubsavas@gmail.com>
2025-03-03 19:35:39 +0200

    Update score.py
    Keeping the high score !

--- score.py
+++ score.py
@@ -6,20 +6,33 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0,270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score} ", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} - High Score: {self.high_score} ", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.hideturtle()
-        self.goto(0,0)
-        self.write("Game Over!", align= ALIGNMENT, font=("Courier", 48, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as write_data:
+                write_data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+
+    # def game_over(self):
+    #     self.hideturtle()
+    #     self.goto(0,0)
+    #     self.write("Game Over!", align= ALIGNMENT, font=("Courier", 48, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+
